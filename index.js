0c945d0a74178061ce3723f137da7276f9544e8b
Matt Coston <mattcoston7@gmail.com>
2020-10-13 16:43:02 -0500

    Fixed delete issue with route

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 
 const express = require('express')
-const bodyParser = require('body-')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
-
+app.use(bodyParser.json())
 const { users } = require('./state')
-
 /* BEGIN - create routes here */
 app.get('/users/:userId', function (req, res) {
   let userId = parseInt(req.params.userId)
   let user = users.find(user => user._id === userId)
   res.send(user)
 })
-
 app.post('/users', function (req, res) {
   //create new user object to array of users and then return new user
   let user = req.body
@@ -20,22 +18,18 @@ app.post('/users', function (req, res) {
   users.push(user)
   res.send(users)
 })
-
-app.put('/users/1', function (req, res) {
+app.put('/users/', function (req, res) {
   let user = users[0]
   user.name = "Dan"
   
   res.send(users[0])
 })
-
 app.delete('/users/:userid', (req, res)=> {
-  res.send(users.splice(1,1))
-})
-
-
-
-
+  let userId = parseInt(req.params.userId)
+  let user = user.find(user => user._id === userId)
+  user.isActive = false
+  res.send('delete')
+});
 /* END - create routes here */
-
 app.listen(port, () => 
-  console.log(`Examn port ${port}!`))
+  console.log(`Example app listening on  ${port}!`))
